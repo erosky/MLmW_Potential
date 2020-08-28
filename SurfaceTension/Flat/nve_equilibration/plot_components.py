73a5f70016c15a1877d29daad9b20c26c0de9ae1
Elise Rosky <emrosky@mtu.edu>
2020-08-27 22:56:53 -0400

    new plots

---
+++ SurfaceTension/Flat/nve_equilibration/plot_components.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = np.loadtxt("components_6A.dat")
+data = np.transpose(data)
+z = data[0][:-1]
+print z
+number = np.roll(data[6][1:],15)
+tangential = np.negative(np.roll(data[5][:-1],15))
+normal = np.negative(np.roll(data[4][:-1],15))
+
+
+plt.figure(1)
+plt.xlabel('Angstrom')
+plt.ylabel('Number')
+plt.plot(z, number, color='#000066')
+plt.savefig('number.png', dpi=200)
+
+plt.figure(2)
+plt.xlabel('Angstrom')
+plt.ylabel('Pressure (atm)')
+plt.plot(z, tangential, color='#000066', label="tangential")
+plt.plot(z, normal, color='#cc99ff', label="normal")
+plt.legend()
+plt.savefig('pressure.png', dpi=200)
+
