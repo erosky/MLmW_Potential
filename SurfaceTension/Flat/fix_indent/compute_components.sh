f97f4df7b64c26624858c04ef9da06016ed22a31
Elise Rosky <emrosky@mtu.edu>
2020-10-28 23:16:57 -0400

    tried surface tension sims with indentor on one interface, and free surface at the other

--- SurfaceTension/Flat/fix_indent/compute_components.sh
+++ SurfaceTension/Flat/fix_indent/compute_components.sh
@@ -2,9 +2,9 @@
 
 # V = thickness*50*50 (volume of one slice)
 
-output=components_indent15.dat
+output=components_halfindent0p1.dat
 
-dumpfile=prod.slab_289K_indent15.dump
+dumpfile=prod.slab_289K_halfindent0p1.dump
 
 volume=15000
 
--- SurfaceTension/Flat/fix_indent/integral.sh
+++ SurfaceTension/Flat/fix_indent/integral.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-input=components_indent15.dat
+input=components_halfindent0p1.dat
 
-output=integral_indent15.txt
+output=integral_halfindent0p1.txt
 
 Z=100
+H=$(expr $Z / 2)
 dz=1
 
+echo 'top and bottom: ' >> $output
 awk 'BEGIN { I = 0 } { I += $8 } END { print I/2 }' $input >> $output
+
+echo 'top: ' >> $output
+awk -v H=$H 'BEGIN { I = 0 } {if (NR > H) { I += $8 }} END { print I }' $input >> $output
+
+echo 'bottom: ' >> $output
+awk -v H=$H 'BEGIN { I = 0 } {if (NR < H) { I += $8 }} END { print I }' $input >> $output
