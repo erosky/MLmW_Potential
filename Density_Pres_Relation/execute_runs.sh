6f7e62baed896c81cd964ce8a036debcf550dad4
Elise Rosky <emrosky@mtu.edu>
2021-08-24 14:35:12 -0400

    prepared runs for gabe to start

--- Density_Pres_Relation/execute_runs.sh
+++ Density_Pres_Relation/execute_runs.sh
@@ -7,13 +7,26 @@
 # PRESSURES:
 # m1000 m950 m900 m850 m800 m750 m700 m650 m600 m550 m500 m450 m400 m350 m300 m250 m200 m150 m100 m50 p0 p1 p50 p100 p150 p200 p250 p300 p350 p400 p450 p500
 
-for TEMP in 205
+for T in 205
 do
-	cd $TEMP
-	for PRES in m1000 m950 m900 m850 m800 m750 m700 m650 m600 m550 m500
+	cd $T
+	for PRES in p1 p50 p100 p150 p200 p250 p300 p350 p400 p450 p500
 	do
 		cd $PRES
-		~/lammps_source/lammps-29Oct20/src/lmp_serial -in in.liquid_density
+		mpirun -np 6 ~/LAMMPS_Source/lammps/src/lmp_mpi -in in.liquid_density
+		cd ..
+	done
+	cd ..
+done
+
+
+for TEMP in 210 215 220 225 230
+do
+	cd $TEMP
+	for PRES in m1000 m950 m900 m850 m800 m750 m700 m650 m600 m550 m500 m450 m400 m350 m300 m250 m200 m150 m100 m50 p0 p1 p50 p100 p150 p200 p250 p300 p350 p400 p450 p500
+	do
+		cd $PRES
+		mpirun -np 6 ~/LAMMPS_Source/lammps/src/lmp_mpi -in in.liquid_density
 		cd ..
 	done
 	cd ..
--- Density_Pres_Relation/execute_runs_b.sh
+++ Density_Pres_Relation/execute_runs_b.sh
@@ -10,10 +10,10 @@
 for TEMP in 205
 do
 	cd $TEMP
-	for PRES in m450 m400 m350 m300 m250 m200 m150 m100 m50 p0 p1 p50 p100 p150 p200 p250 p300 p350 p400 p450 p500
+	for PRES in p0
 	do
 		cd $PRES
-		~/lammps_source/lammps-29Oct20/src/lmp_serial -in in.liquid_density
+		mpirun -np 6 ~/lammps_source/lammps-29Oct20/src/lmp_mpi -in in.liquid_density
 		cd ..
 	done
 	cd ..
