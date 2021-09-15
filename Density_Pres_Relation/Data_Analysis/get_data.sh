2ed5af3a0473027d81cc2574c43cc6734f51fc4c
Elise Rosky <emrosky@mtu.edu>
2021-09-14 22:55:56 -0400

    pres v dens analysis

--- Density_Pres_Relation/Data_Analysis/get_data.sh
+++ Density_Pres_Relation/Data_Analysis/get_data.sh
@@ -7,11 +7,12 @@
 # m1000 m950 m900 m850 m800 m750 m700 m650 m600 m550 m500 m450 m400 m350 m300 m250 m200 m150 m100 m50 p0 p1 p50 p100 p150 p200 p250 p300 p350 p400 p450 p500
 
 cd ..
-for TEMP in 205 210 215 220
+for TEMP in 205 210 215 220 225
 do
 	cd $TEMP
 	touch pres_density.dat
-	for PRES in m1000 m950 m900 m850 m800 m750 m700 m650 m600 m550 m500 m450 m400 m350 m300 m250 m200 m150 m100 m50 p0
+	truncate -s 0 pres_density.dat
+	for PRES in m1000 m950 m900 m850 m800 m750 m700 m650 m600 m550 m500 m450 m400 m350 m300 m250 m200 m150 m100 m50 p0 p1 p50 p100 p150 p200 p250 p300 p350 p400 p450 p500
 	do
 		cd $PRES
 		start=500
